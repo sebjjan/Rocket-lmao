8232ae13be667c6977d2bc920bff481de9c9cb8a
Mullvadsmacka <joel@monstermatte.se>
2023-06-05 12:29:26 +0200

    lerp commit

---
+++ Rocket lmao/Assets/Scenes/LegAnimation.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class LegAnimation : MonoBehaviour
+{
+    public GameObject target1;
+    public GameObject target2;
+    public float lerpSpeed;
+    public float maxDistance;
+    public GameObject balance;
+    public Collider2D searchRange;
+    public ContactFilter2D contactFilter;
+
+    Vector3 footTarget;
+
+   public float lerp = 0;
+    private Vector3 oldPos;
+    private Vector3 newPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FindTarget();
+
+
+
+
+        if (Vector3.Distance(target1.transform.position, balance.transform.position) > maxDistance && lerp >= 1)
+        {
+            lerp = 0;
+
+            Debug.Log("out of balance");
+
+           
+
+        }
+
+        MoveFoot();
+    }
+
+
+    private void MoveFoot()
+    {
+        if(lerp < 0)
+        {
+            oldPos = target1.transform.position;
+            newPos = footTarget;
+
+            target1.transform.position = Vector3.Lerp(oldPos, newPos, lerp);
+            lerp += Time.deltaTime * lerpSpeed;
+        }
+      
+
+        //fixa den
+        //target1.transform.position.y = Mathf.Sin(lerp);
+
+    }
+
+    private void FindTarget()
+    {
+        Collider2D closestCollider = null;
+        List<Collider2D> colliders = new List<Collider2D>();
+        searchRange.OverlapCollider(contactFilter, colliders);
+        foreach(Collider2D c in colliders)
+        {
+            if(closestCollider == null)
+            {
+                closestCollider = c;
+            }
+            else if(Vector3.Distance(balance.transform.position, c.ClosestPoint(balance.transform.position)) < Vector3.Distance(balance.transform.position, closestCollider.ClosestPoint(balance.transform.position)))
+                    {
+                closestCollider = c;
+            }
+        }
+        if(closestCollider != null)
+        {
+    footTarget = closestCollider.ClosestPoint(balance.transform.position);
+        }
+    
+       // Vector3 target = Physics2D.Linecast()
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(footTarget, 2);
+
+        Gizmos.DrawSphere(oldPos, 2);
+
+
+        Gizmos.DrawSphere(newPos, 2);
+    }
+
+}
